ce70e51f298cabec53c160750e3170d54362d245
Mohammed320 <53355065+Mohammed320@users.noreply.github.com>
2020-06-20 02:52:34 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const fs = require('fs'); 
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Welcome Bro ${client.user.tag}!`);
+});
+
+client.user.setPresence({
+  status: 'idle',
+  game: { 
+     type: 0,
+     name: 'Im better by myself..',
+     details: `Smile`,
+     state: 'Im used to the pain', 
+    application_id: '626146017472937994',
+        smallImageKey: '',
+        smallImageText: '',
+        LargeImageKey: '',
+        LargeImageText: '', 
+        instance:false, 
+   }
+    });
+});
+
+
+
+
+client.login(process.env.BOT_TOKEN)
