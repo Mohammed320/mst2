eefe02d8420d56aa7d1ca8785d8d45bdb5628d6f
Mohammed320 <53355065+Mohammed320@users.noreply.github.com>
2020-06-20 03:06:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const fs = require('fs'); 
+client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Welcome Bro ${client.user.tag}!`);
